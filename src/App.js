81f0cd05667b4c8d4e28a210a14f9c91354d6bb3
fahad-zakir <fahadzakir81@gmail.com>
2020-11-17 18:30:34 -0600

    almost-done-need-finishing-touches-functionality-working

--- src/App.js
+++ src/App.js
@@ -5,48 +5,51 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      subhanallah: null,
-      alhumdullilah: null,
-      allahuakbar: null
+      subhanallah: 0,
+      alhumdullilah: 0,
+      allahuakbar: 0
     }
   }
 
-  // subhanllahChange = (e) => {
-  // e.preventDefault()
-  // this.setState({
-  //   subhanallah: subhanllah++
-  // })
+  subhanllahChange = (e) => {
+  e.preventDefault()
+  this.setState({
+    subhanallah: this.state.subhanallah + 1
+  })
 
-  // }
-  // alhumdullilahChange = (e) => {
-  // e.preventDefault()
-  // this.setState({
-  //   alhumdullilah: alhumdullilah++
-  // })
+  }
+  alhumdullilahChange = (e) => {
+  e.preventDefault()
+  this.setState({
+    alhumdullilah: this.state.alhumdullilah + 1
+  })
 
-  // }
-  // allahuakbarChange = (e) => {
-  // e.preventDefault()
-  // this.setState({
-  //   allahuakbar: allahkuakbar++
-  // })
-  // }
+  }
+  allahuakbarChange = (e) => {
+  e.preventDefault()
+  this.setState({
+    allahuakbar: this.state.allahuakbar + 1
+  })
+  }
 
   render(){
-  const subhanallah = this.state
-  const alhumdullilah = this.state
-  const allahuakbar = this.state
+  const { subhanallah } = this.state
+  const { alhumdullilah } = this.state
+  const { allahuakbar } = this.state
 
     return(
       <div className="container">
-        <div>
-      <img src="assets/subhanallah.jpg" alt="subhanallah" />
+      <div>
+      <img src="assets/subhanallah.jpg" alt="subhanallah" onClick={this.subhanllahChange} />
+        <button className="cool"  type="text" id="count" >{this.state.subhanallah}</button>
       </div>
       <div>
-      <img src="assets/alhumdullilah.jpg" alt="alhumdullilah"  />
+      <img src="assets/alhumdullilah.jpg" alt="alhumdullilah" onClick={this.alhumdullilahChange}  />
+              <button className="cool" type="text" id="count">{this.state.alhumdullilah}</button>
       </div>
       <div>
-      <img src="assets/allahuakbar.jpg" alt="allahuakbar"  />
+      <img src="assets/allahuakbar.jpg" alt="allahuakbar" onClick={this.allahuakbarChange}  />
+              <button className="cool" type="text" id="count">{this.state.allahuakbar}</button>
       </div>
       </div>
     )
